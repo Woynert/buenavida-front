c9641f4945f90c6e84c2fc4bd9c3e9b5ec9cab0f
woynert <whayner.porras.2020@upb.edu.co>
2022-11-07 13:12:30 -0500

    Double price slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 // vitrina
@@ -17,22 +17,24 @@ import { ViewSigninComponent } from '@layout/view-signin/view-signin.component';
 import { HeaderComponent } from '@shared/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ViewVitrinaComponent,
-    HeaderComponent,
-    ViewCartComponent,
-    ViewLoginComponent,
-    ViewSigninComponent,
-    VitrinaFilterComponent,
-    VitrinaPagerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-	HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ViewVitrinaComponent,
+		HeaderComponent,
+		ViewCartComponent,
+		ViewLoginComponent,
+		ViewSigninComponent,
+		VitrinaFilterComponent,
+		VitrinaPagerComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/layout/view-vitrina/vitrina-filter/vitrina-filter.component.ts
+++ src/app/layout/view-vitrina/vitrina-filter/vitrina-filter.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-vitrina-filter',
-  templateUrl: './vitrina-filter.component.html',
-  styleUrls: ['./vitrina-filter.component.css']
+	selector: 'app-vitrina-filter',
+	templateUrl: './vitrina-filter.component.html',
+	styleUrls: ['./vitrina-filter.component.css']
 })
 export class VitrinaFilterComponent implements OnInit {
 
-  constructor() { }
+	minPrice  : number = 0;
+	maxPrice  : number = 100;
+	leftPrice : number = 0;
+	rightPrice: number = 100;
 
-  ngOnInit(): void {
-  }
+	@ViewChild('rangeLeft') rangeLeft!: ElementRef<HTMLInputElement>;
+	@ViewChild('rangeRight') rangeRight!: ElementRef<HTMLInputElement>;
+	@ViewChild('progressBar') progressBar!: ElementRef<HTMLDivElement>;
 
+	constructor(
+		private formBuilder: FormBuilder
+	) { }
+
+	ngOnInit(): void {
+	}
+
+	updateDoubleRange() {
+		let leftPrice = parseInt(this.rangeLeft.nativeElement.value);
+		let rightPrice = parseInt(this.rangeRight.nativeElement.value);
+
+		if (leftPrice > rightPrice){
+			leftPrice = rightPrice;
+			this.rangeLeft.nativeElement.value = rightPrice + "";
+		}
+
+		if (rightPrice < leftPrice){
+			rightPrice = leftPrice;
+			this.rangeRight.nativeElement.value = leftPrice + "";
+		}
+
+		// move progress bar
+		let leftMargin = (leftPrice * 100) / this.maxPrice;
+		this.progressBar.nativeElement.style.left = leftMargin + "%";
+		let rightMargin = 100 - (rightPrice * 100) / this.maxPrice;
+		this.progressBar.nativeElement.style.right = rightMargin + "%";
+
+		this.leftPrice = leftPrice;
+		this.rightPrice = rightPrice;
+	}
+
+	submitPriceFilter(){
+	}
 }
