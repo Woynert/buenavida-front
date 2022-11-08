77e8c33d912f1b084695c5ba0fb89374a18d1f32
andres <andres@debian.andres>
2022-11-08 00:03:30 -0500

    Advance Modal Cart

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit, ViewChild, ElementRef,HostListener } from '@angular/core';
 import { SearchService, SearchResponse } from '@service/search.service';
 
+import { ProductsCart } from '@shared/ProductsCart';
+
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css']
 })
+
+
+
 export class HeaderComponent implements OnInit {
 
 	@ViewChild("searchBarInput") searchBarInput!: ElementRef<HTMLInputElement>;
-	public styleDisplay = "display:none";
+	public styleDisplay:string = "display:none";
+
+	/*Modal Cart Variables*/
+	public styleDisplayModalCart:string = "display:none";
+	public total_productos:number = 0;
+	public productsCart: ProductsCart[] = [];
 
 	constructor(
 		public searchService: SearchService
@@ -32,5 +42,23 @@ export class HeaderComponent implements OnInit {
 
 	hideMyMenu(): void {
 		this.styleDisplay = "display:none";
+	}
+
+	openModalCart(): void {
+		this.styleDisplayModalCart = "display:block";
+		if (localStorage.getItem("cart") != null) {
+			let products = JSON.parse(localStorage.getItem("cart") || "")
+			let total = 0;
+			this.productsCart = products;
+			for (let i = 0; i < products.length; i++) {
+				let element = products[i];
+				total += element.quantity;
+			}
+			this.total_productos = total;
+		}
+	}
+
+	closeModalCart(): void {
+		this.styleDisplayModalCart = "display:none";
 	}
 }
