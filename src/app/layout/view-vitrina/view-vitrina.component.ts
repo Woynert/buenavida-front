6938a4130f829395f487d1dc3d0e7086f8b29f9e
woynert <whayner.porras.2020@upb.edu.co>
2022-11-09 14:10:17 -0500

    Merge with dev

--- src/app/layout/view-vitrina/view-vitrina.component.ts
+++ src/app/layout/view-vitrina/view-vitrina.component.ts
@@ -9,12 +9,16 @@ import { CartService } from '@service/cart.service';
 @Component({
 	selector: 'app-view-vitrina',
 	templateUrl: './view-vitrina.component.html',
-	styleUrls: ['./view-vitrina.component.css']
+	styleUrls: [
+		'./view-vitrina.component.css',
+		'./modal-product.css',
+	]
 })
 export class ViewVitrinaComponent implements OnInit {
 
 	searchTermSubscription: Subscription = Subscription.EMPTY;
 
+	selectedProduct: Product | null = null;
 	products    : Product[] = [];
 	totalPages  : number  = 0;
 	searchTerm  : string = "";
@@ -63,8 +67,70 @@ export class ViewVitrinaComponent implements OnInit {
 		this.getProductsFromSearch();
 	}
 
-	addProduct(product: Product){
+	// favorites
+
+	async addToFavorites(itemid: string) {
+		await this.favoritesService.addToFavorites(itemid);
+		await this.favoritesService.fetchUserInfo();
+		this.matchFavorites();
+	}
+
+	async removeFromFavorites(itemid: string) {
+		await this.favoritesService.removeFromFavorites(itemid);
+		await this.favoritesService.fetchUserInfo();
+		this.matchFavorites();
+	}
+
+	// tag some products as favorite
+
+	matchFavorites() {
+
+		if (!this.favoritesService.currentUser)
+			return;
+
+		const userInfo: iUserInfo = this.favoritesService.currentUser!;
+
+		if (userInfo.favorites){
+
+			// iterate products
+			this.products.forEach((product: Product) => {
+				product.favorite = userInfo.favorites.some((fav: Product) => {
+					return (product._id == fav._id);
+				});
+			});
+		}
+		else{
+
+			// iterate products
+			this.products.forEach((product: Product) => {
+				product.favorite = false;
+			});
+		}
+
+	}
+
+	addProduct (product: Product){
 		this.cartService.addProduct(product,1);
 	}
+
+	// product modal
+
+	selectProduct (product: Product){
+		this.selectedProduct = product;
+
+		// set quantity
+
+		// check is already on cart
+		// 		get quantity
+		// else
+		// 		quantity = 1
+		//
+	}
+
+	deselectProduct (){
+		this.selectedProduct = null;
+	}
+
+	
 
 }
